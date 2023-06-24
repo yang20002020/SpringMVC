37ebbcd452d019b23d9ce10342363765bbfd84b4
yang20002020 <2628636987@qq.com>
2023-06-24 16:38:43 +0800

    bean加载控制  简化开发

---
+++ springMVC_04_bean_load/src/main/java/com/fuyu/config/ServletContainersInitConfig.java
@@ -0,0 +1,41 @@
+package com.fuyu.config;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
+
+
+//public class ServletContainersInitConfig extends AbstractDispatcherServletInitializer {
+//    protected WebApplicationContext createServletApplicationContext() {
+//        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+//        ctx.register(SpringMvcConfig.class);
+//        return ctx;
+//    }
+//    protected WebApplicationContext createRootApplicationContext() {
+//        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+//        ctx.register(SpringConfig.class);
+//        return ctx;
+//    }
+//    protected String[] getServletMappings() {
+//        return new String[]{"/"};
+//    }
+//
+//}
+public class ServletContainersInitConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{SpringConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
---
+++ springMVC_04_bean_load/src/main/java/com/fuyu/config/SpringConfig.java
@@ -0,0 +1,13 @@
+package com.fuyu.config;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Controller;
+
+@Configuration
+@ComponentScan(value="com.fuyu",excludeFilters = @ComponentScan.Filter(
+        type= FilterType.ANNOTATION,
+        classes= Controller.class
+))
+public class SpringConfig {
+}
