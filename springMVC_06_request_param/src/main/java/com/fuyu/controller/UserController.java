1d585594b9fee16feec591ecedc5d766b064d90b
yang20002020 <2628636987@qq.com>
2023-06-25 10:41:19 +0800

    post请求和get请求发送普通参数以及中文乱码的处理

---
+++ springMVC_06_request_param/src/main/java/com/fuyu/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.fuyu.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+//请求参数
+@Controller
+public class UserController {
+
+    //普通参数：请求参数与形参名称对应即可完成参数传递
+    @RequestMapping("/commonParam")
+    @ResponseBody
+    public String commonParam(String name, int age) {
+        System.out.println("普通参数传递 name ==> " + name);
+        System.out.println("普通参数传递 age ==> " + age);
+        return "{'module':'common param'}";
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
