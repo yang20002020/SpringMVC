0d4fcb19156142d7d6fb75598b7ec6e1d4443ad0
yang20002020 <2628636987@qq.com>
2023-06-27 08:44:32 +0800

    SSM 功能模块开发

---
+++ springMVC_017_SSM/src/main/java/com/fuyu/config/SpringMvcConfig.java
@@ -0,0 +1,11 @@
+package com.fuyu.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan("com.fuyu.controller")
+@EnableWebMvc
+public class SpringMvcConfig {
+}
