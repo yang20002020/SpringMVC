1bab1cd014fb02576252f24539bd3a2002cef6b2
yang20002020 <2628636987@qq.com>
2023-06-29 09:08:21 +0800

    拦截器入门案例

---
+++ springMVC_027_interceptor/src/main/java/com/fuyu/config/SpringMvcConfig.java
@@ -0,0 +1,13 @@
+package com.fuyu.config;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@ComponentScan({"com.fuyu.controller","com.fuyu.config"})
+@EnableWebMvc
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+
+}
