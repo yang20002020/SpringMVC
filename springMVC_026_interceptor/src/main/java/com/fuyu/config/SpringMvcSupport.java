9191a46ada206f2ea457fa09f4a8b407b291566f
nil2000 <2628636987@qq.com>
2023-11-26 09:26:18 +0800

    拦截器

--- springMVC_026_interceptor/src/main/java/com/fuyu/config/SpringMvcSupport.java
+++ springMVC_026_interceptor/src/main/java/com/fuyu/config/SpringMvcSupport.java
@@ -17,12 +17,14 @@ public class SpringMvcSupport extends WebMvcConfigurationSupport {
     //静态页面访问
     @Override
     protected  void addResourceHandlers(ResourceHandlerRegistry registry){
+        //当访问/pages/????时候，不要走springMVC, 而是从/pages目录下查找内容
         registry.addResourceHandler("/pages/**").addResourceLocations("/pages");
     }
 
     //拦截器
     @Override
     protected  void addInterceptors(InterceptorRegistry registry){
+        //对/books进行拦截处理
         registry.addInterceptor(projectInterceptor).addPathPatterns("/books");
     }
 
